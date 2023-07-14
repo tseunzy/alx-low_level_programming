cb46a56ac485581ae02769ea482f20d52a8d250e
tseunzy <Oluwaseun Ogundare>
2023-07-14 16:58:00 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Aways 0 (success)
+*/
+int main(void)
+{
+	int num;
+
+	while (num < 10)
+	{
+		printf("%d\n", num);
+		num += 1;
+	}
+	return (0);
+}
+
