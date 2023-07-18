e25129bade45c478d37a501dc480b4758233702f
tseunzy <Oluwaseun Ogundare>
2023-07-18 15:34:24 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Return:0
+ */
+void print_alphabet_x10(void)
+{
+	char num = '0';
+	char alph;
+
+	while (num <= 9)
+	{
+		alph = 'z';
+
+		while (alph <= 'z')
+		{
+			_putchar(alph);
+			alph++;
+		}
+		_putchar(num);
+		num++;
+		_putchar('\n');
+	}
+}
