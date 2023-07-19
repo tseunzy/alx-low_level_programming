95728c6992574da28743915aeb9c63df9413fe35
tseunzy <Oluwaseun Ogundare>
2023-07-19 13:05:11 -0700

    0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is %s\n", i, "positive");
+	else if (i < 0)
+		printf("%d is %s\n", i, "negative");
+	else
+		printf("%d is %s\n", i, "zero");
+	return (0);
+}
