caa4d7aff6c847b7e75c4c2c0f5a4bf894c44093
tseunzy <Oluwaseun Ogundare>
2023-07-20 16:40:59 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,8 @@
 
 /**
  * main - program that prints the numbers from 1 to 100, followed by a new line. 
- *	  But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz
+ * But for multiples of three print Fizz instead of the number 
+ * for the multiples of five print Buzz
  * Return: Always 0
  */
 
