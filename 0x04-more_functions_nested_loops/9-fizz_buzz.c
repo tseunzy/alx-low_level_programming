52c2d45699e706e8bc36bc017ce5c83211d1437f
tseunzy <Oluwaseun Ogundare>
2023-07-20 16:26:28 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,20 +13,24 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if ((num % 3) == 0 && (num % 5) == 0)
-			printf("FizzBuzz");
+		{
+			printf("FizzBuzz ");
+		}
 
 		else if ((num % 3) == 0)
-			printf("Fizz");
+		{
+			printf("Fizz ");
+		}
 
 		else if ((num % 5) == 0)
-			printf("Buzz");
+		{
+			printf("Buzz ");
+		}
 
 		else
-			printf("%d", num);
-
-		if (num == 100)
-			continue;
-		printf(" ");
+		{
+			printf("%d ", num);
+		}
 	}
 	printf("\n");
 	return (0);
