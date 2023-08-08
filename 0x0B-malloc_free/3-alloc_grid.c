7e3f12ad83124b347e2882e2cd2b8517b48602f2
tseunzy <tseunzy@yahoo.com>
2023-08-08 12:20:30 +0100

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -37,12 +37,10 @@ int **alloc_grid(int width, int height)
 			free(mee);
 			return (NULL);
 		}
-	}
-
-	for (x = 0; x < height; x++)
-	{
 		for (y = 0; y < width; y++)
+		{
 			mee[x][y] = 0;
+		}
 	}
 
 	return (mee);
