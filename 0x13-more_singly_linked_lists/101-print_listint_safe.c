0b857b324b8daf05223f7073f9e383be5e9161c7
tseunzy <tseunzy@yahoo.com>
2023-08-29 00:46:12 +0100

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,61 +1,35 @@
 #include "lists.h"
-
-int node_is_in_list(const listint_t *node, const listint_t *list[]);
-const listint_t *print_node(const listint_t *node);
-size_t print_listint_safe(const listint_t *head);
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * node_is_in_list - Checks if a node is in a list
- * @node: The node to check
- * @list: The list to check in
- * Return: 1 if the node is in the list, 0 otherwise
- */
-int node_is_in_list(const listint_t *node, const listint_t *list[])
-{
-	int i;
-
-	for (i = 0; list[i] != NULL; i++)
-	{
-		if (list[i] == node)
-			return (1);
-	}
-	return (0);
-}
-
-/**
- * print_node - Prints a node
- * @node: The node to print
- * Return: The address of the node
- */
-const listint_t *print_node(const listint_t *node)
-{
-	printf("[%p] %d\n", (void *)node, node->n);
-	return (node);
-}
-
-/**
- * print_listint_safe - Prints a listint_t linked list
- * @head: The list
- * Return: The number of nodes in the list
+ * print_listint_safe - prints all elements of a listint_t linked list
+ * @head: pointer to the head of the list
+ * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *visited[1024];
-	int i;
+	size_t i, nodes = 0;
+	const listint_t *current = head;
+	const listint_t *array[1024] = {NULL};
 
 	if (head == NULL)
 		exit(98);
-	for (i = 0; head != NULL && i < 1024; i++)
+	while (current)
 	{
-		if (node_is_in_list(head, visited))
+		for (i = 0; i < nodes; i++)
 		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			return (i);
+			if (array[i] == current)
+			{
+				printf("-> [%p] %d\n", (void *)current, current->n);
+				return (nodes);
+			}
 		}
-		visited[i] = print_node(head);
-		head = head->next;
+		printf("[%p] %d\n", (void *)current, current->n);
+		array[nodes] = current;
+		nodes++;
+		current = current->next;
 	}
-	visited[i] = NULL;
-	return (i);
+	return (nodes);
 }
 
