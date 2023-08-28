73f0a08ffd3093b6e47e722f641751c9aa13cc70
tseunzy <tseunzy@yahoo.com>
2023-08-29 00:11:53 +0100

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		nodes++;
 		h = h->next;
 	}
+
 	return (nodes);
 }
