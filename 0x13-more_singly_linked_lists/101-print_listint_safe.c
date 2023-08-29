abd558cae8affa400bc7f8a0cfe287c94e5a62f1
tseunzy <tseunzy@yahoo.com>
2023-08-29 21:51:21 +0100

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,35 +1,61 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
+
+size_t find_loop(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 
 /**
- * print_listint_safe - prints all elements of a listint_t linked list
- * @head: pointer to the head of the list
- * Return: the number of nodes in the list
+ * find_loop - Finds the loop in a listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: The number of nodes before the loop starts.
+ * If there is no loop, returns 0.
+ */
+size_t find_loop(const listint_t *head)
+{
+	const listint_t *slow = head, *fast = head;
+	size_t nodes = 0;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		nodes++;
+
+		if (slow == fast)
+		{
+			return (nodes);
+		}
+	}
+
+	return (0);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i, nodes = 0;
-	const listint_t *current = head;
-	const listint_t *array[1024] = {NULL};
+	size_t nodes = 0, loop_start;
 
-	if (head == NULL)
-		exit(98);
-	while (current)
+	loop_start = find_loop(head);
+
+	while (head)
 	{
-		for (i = 0; i < nodes; i++)
-		{
-			if (array[i] == current)
-			{
-				printf("-> [%p] %d\n", (void *)current, current->n);
-				return (nodes);
-			}
-		}
-		printf("[%p] %d\n", (void *)current, current->n);
-		array[nodes] = current;
+		printf("[%p] %d\n", (void *)head, head->n);
 		nodes++;
-		current = current->next;
+		head = head->next;
+
+		if (loop_start == nodes)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
 	}
+
 	return (nodes);
 }
 
