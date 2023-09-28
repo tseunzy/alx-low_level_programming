8382894549a1f5f413fc0275e5f4fb56cb898aa4
tseunzy <tseunzy@yahoo.com>
2023-09-28 14:27:56 +0100

    task0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,7 +9,7 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(*n) * 8) || n == NULL)
+	if (index >= (sizeof(unsigned long int) * 8)
 		return (-1);
 
 	*n = (*n & ~(1UL << index));
